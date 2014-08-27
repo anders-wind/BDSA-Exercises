1d4bb7bf96ff85c7924c8e76af4378b9b808d252
Anders Wind Steffensen <awia00@gmail.com>
2014-08-27 23:45:24 +0200

    Fixed HelloWorld Added a solution for isPowerOf

---
+++ Wind_Projects/IsPowerOf/IsPowerOf.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsPowerOf
+{
+    class IsPowerOf
+    {
+        /**
+         * Has problems because it uses ints (28,3 returns true)
+         */
+        static bool IsPowerOfBad(int a, int b)
+        {
+            if (a == b) return true;
+            if (a > b)
+            {
+                if (IsPowerOfBad(a / b, b) == true) return true;
+            }
+            return false;
+        }
+
+        static bool IsPowerOfGood(int a, int b, int originalB)
+        {
+            if (a == b) return true;
+            if (a > b)
+            {
+                if (IsPowerOfGood(a, originalB * b, originalB) == true) return true;
+            }
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            if(args.Length > 1)
+            {
+                Console.WriteLine(Convert.ToInt32(args[0]) + " " + Convert.ToInt32(args[1]));
+                int args1 = Convert.ToInt32(args[0]);
+                int args2 = Convert.ToInt32(args[1]);
+                Console.WriteLine("\nBad method");
+                Console.WriteLine("Is " + args1 + " a power of " + args2 + "? " + IsPowerOfBad(args1, args2));
+                Console.WriteLine("\nGood method");
+                Console.WriteLine("Is " + args1 + " a power of " + args2 + "? " + IsPowerOfGood(args1, args2, args2));
+                Console.WriteLine();
+            }
+        }
+    }   
+}
---
+++ Wind_Projects/Wind_HelloWorld/CommandLine.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wind_HelloWorld
+{
+    public class CommandLine
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Wind");
+            Console.ReadKey();
+
+            new CommandLine();
+            Console.ReadKey();
+
+            Console.WriteLine("\nFor loop");
+            Console.WriteLine("Number of command line parameters = {0}", args.Length);
+            for(int i = 0; i<args.Length ; i++)
+            {
+                Console.WriteLine("Arg[{0}] = [{1}]", i, args[i]);
+            }
+            Console.ReadKey();
+
+            Console.WriteLine("\nForeach");
+            foreach (string s in args)
+            {
+                Console.WriteLine(s);
+            }
+            Console.ReadKey();
+        }
+
+        public CommandLine()
+        {
+            writeHelloWind5Times();
+        }
+
+        private void writeHelloWind5Times()
+        {
+            for(int i = 0 ; i<5 ;i++)
+            {
+                Console.WriteLine("\nHello Wind");
+            }
+        }
+    }
+}
