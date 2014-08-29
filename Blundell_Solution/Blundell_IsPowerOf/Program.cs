0b5ea85728da7d66baf50e3bb1d76ca87b968d03
Christopher Blundell <christopher.n.blundell@gmail.com>
2014-08-29 12:27:29 +0200

    Created IsPowerOf by Christopher

---
+++ Blundell_Solution/Blundell_IsPowerOf/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blundell_IsPowerOf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
