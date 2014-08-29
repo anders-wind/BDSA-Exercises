82a6d7dfcc5a792a67aefcae9f1ec928aaceda59
Christopher Blundell <christopher.n.blundell@gmail.com>
2014-08-29 12:33:27 +0200

    Created project IsPowerOf

---
+++ BDSA2014/IsPowerOf/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsPowerOf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
