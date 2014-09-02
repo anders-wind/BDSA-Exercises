f4be24c6fdd123d796f352a040eb447033d4fb2c
Anders Wind Steffensen <awis@itu.dk>
2014-09-02 14:04:34 +0200

    Fixed the hello world project fuck bug

---
+++ BDSA2014/HelloWorld/SayHelloWorld.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class SayHelloWorld
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+
+            Console.WriteLine("Number of command line parameters = {0}", args.Length);
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine("Arg[{0}] = [{1}]", i, args[i]);
+            }
+
+            foreach (string s in args)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
