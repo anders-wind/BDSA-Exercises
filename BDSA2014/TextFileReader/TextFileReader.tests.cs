7bd8c9453e6bc1681479a59a5dc652da4f6c4ac4
Anders Wind Steffensen <awis@itu.dk>
2014-09-15 17:51:35 +0200

    added a test class for our textfilereader

---
+++ BDSA2014/TextFileReader/TextFileReader.tests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TextFileReader
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [TestFixture]
+    class TextFileReaderTests
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [Test]
+        public void TestNullInput()
+        {
+        }
+    }
+}
