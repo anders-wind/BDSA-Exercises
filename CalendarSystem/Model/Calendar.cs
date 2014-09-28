7e13d92201b35a5d7fc4d64fe036c505023c553e
Anders Wind Steffensen <awia00@gmail.com>
2014-09-28 15:37:50 +0200

    Created observer pattern between calendar and view

--- CalendarSystem/Model/Calendar.cs
+++ CalendarSystem/Model/Calendar.cs
@@ -3,12 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using View;
 
 namespace Model
 {
     public class Calendar
     {
-        List<Event> events = new List<Event>(); 
+        private IList<ISubscriber> subscribers = new List<ISubscriber>();
+        private IList<Event> events = new List<Event>();
 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        private void notifySubscribers()
+        {
+            foreach (var subscriber in subscribers)
+            {
+                subscriber.Notify();
+            }
+        }
+
+        public void Subscribe(ISubscriber subscriber)
+        {
+            subscribers.Add(subscriber);
+        }
     }
+
+
+    
+
 }
