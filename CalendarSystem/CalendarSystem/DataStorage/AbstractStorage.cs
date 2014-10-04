df0a5a98951e00b0fa1e3c90045e34ab86340b68
Anders Wind Steffensen <awia00@gmail.com>
2014-10-04 21:09:16 +0200

    Created the strategy pattern

--- CalendarSystem/CalendarSystem/DataStorage/AbstractStorage.cs
+++ CalendarSystem/CalendarSystem/DataStorage/AbstractStorage.cs
@@ -10,57 +10,50 @@ namespace CalendarSystem.DataStorage
     class AbstractStorage : IAbstractStorage
     {
         private IStorage _storage;
+        public IConnection _connection { private get; set; }
 
-        public AbstractStorage(IStorage storage)
+        public AbstractStorage(IStorage storage, IConnection connection )
         {
             _storage = storage;
+            _connection = connection;
         }
 
         public void loginAuthentication(string userName, string password)
         {
             _storage.loginAuthentication(userName, password);
         }
-
         public void SaveEvent(string description, DateTime dateTime, TimeSpan timeSpan, Notification notification)
         {
             _storage.SaveEvent(description,dateTime,timeSpan,notification);
         }
-
         public void UpdateEvent(int ID, string description, DateTime dateTime, TimeSpan timeSpan, Notification notification)
         {
             _storage.UpdateEvent(ID, description, dateTime, timeSpan, notification);
         }
-
         public IList<IEvent> GetAllEvents()
         {
             _storage.GetAllEvents();
         }
-
         public IEvent GetEvent(int ID)
         {
             _storage.GetEvent(ID);
         }
-
         public IList<IEvent> GetEventsBetweenDates(DateTime beginDateTime, DateTime endDateTime)
         {
             _storage.GetEventsBetweenDates(beginDateTime, endDateTime);
         }
-
         public void CreateTag(Tag tag)
         {
             _storage.CreateTag(tag);
         }
-
         public void NotifyObservers()
         {
             _storage.NotifyObservers();
         }
-
         public void BeObserved(IObserver observer)
         {
             _storage.BeObserved(observer);
         }
-
         public void BeObserved(IList<IObserver> observers)
         {
             _storage.BeObserved(observers);
