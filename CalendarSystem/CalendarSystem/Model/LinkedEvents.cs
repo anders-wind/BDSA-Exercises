05d7789f472410a945b78d190b247dc005d8ef1c
Anders Wind Steffensen <awia00@gmail.com>
2014-10-05 02:48:56 +0200

    Created the Composite pattern

---
+++ CalendarSystem/CalendarSystem/Model/LinkedEvents.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalendarSystem.Model
+{
+    class LinkedEvents : IEvent
+    {
+
+        private IList<IEvent> events = new List<IEvent>();
+        public Notification _notification { get; set; }
+        public DateTime _date { get; set; }
+        public TimeSpan _timeSpan { get; set; }
+        public string _description { get; set; }
+
+        public int _ID { get; private set; }
+
+        public void AddEvent(IEvent anEvent)
+        {
+            events.Add(anEvent);
+        }
+        public void AddRangeOfEvent(IList<IEvent> someEvents)
+        {
+            events.Concat(someEvents);
+        }
+
+        public void changeTag(Tag tag)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
