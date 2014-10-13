bcfcc49d94aa5d7a69728076de8fb4bbceccb83b
Anders Wind Steffensen <awia00@gmail.com>
2014-10-13 22:09:25 +0200

    Tried to create the reporting module

---
+++ Northwind_Solution/Northwind/ReportingController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind
+{
+    class ReportingController
+    {
+        /// <summary>
+        /// Report<IList<OrdersByTotalPriceDto>, ReportError> TopOrdersByTotalPrice(int count)
+        /// </summary>
+        /// <param name="count"></param>
+        /// <returns></returns>
+        Report<IList<Order>, ReportError> TopOrdersByTotalPrice(int count)
+        {
+            IList<Order> topOrders = null;
+            // get top results
+            new Report<IList<Order>, ReportError>(null, topOrders);
+            return null;
+        }
+
+        public class Report<TData, TError>
+        {
+            public TData Data { get; set; }
+            public TError Error { get; set; }
+
+            public Report(TError error, TData data)
+            {
+                Error = error;
+                Data = data;
+            }
+        }
+
+        public class ReportError
+        {
+            public string errorMessage { get; set; }
+
+            public ReportError(string errorMessage)
+            {
+                this.errorMessage = errorMessage;
+            }
+        }
+    }
+}
