a5813d3a0599043445a5cd62c7947bf68e6a7066
Anders Wind Steffensen <awia00@gmail.com>
2014-10-17 00:56:12 +0200

    Fixed the problme with Customer name

--- Northwind_Solution/Northwind/Reporting Module/ReportingController.cs
+++ Northwind_Solution/Northwind/Reporting Module/ReportingController.cs
@@ -27,7 +27,7 @@ namespace Northwind.Reporting_Module
             var ordersByTotalPriceDto = (from order in ListOfOrders
                 let orderId = order.OrderID
                 let orderDate = order.OrderDate
-                let customerContactName = TryGet(order)
+                let customerContactName = order.Customer.ContactName
                 let totalPrice = (from od in order.Order_Details
                                     select od).Sum(od => od.UnitPrice*od.Quantity)
                 let totalPriceWithDiscount = (from od in order.Order_Details
@@ -47,25 +47,13 @@ namespace Northwind.Reporting_Module
             }
         }
 
-        private string TryGet(Order order)
-        {
-            try
-            {
-                return order.Customer.ContactName;
-            }
-            catch (Exception)
-            {
-                return null;
-            }
-        }
+        //public Report<IList<ProductsBySaleDto>, ReportError> TopProductsBySale(int count)
+        //{
+        //    var listOfProducts = _northwindController._products;
 
-        public Report<IList<ProductsBySaleDto>, ReportError> TopProductsBySale(int count)
-        {
-            var listOfProducts = _northwindController._products;
-
-            IList<ProductsBySaleDto> ProductsBySaleDToList = from product in listOfProducts
-                select product.Category;
-        }
+        //    IList<ProductsBySaleDto> ProductsBySaleDToList = from product in listOfProducts
+        //        select product.Category;
+        //}
 
         public class Report<TData, TError>
         {
