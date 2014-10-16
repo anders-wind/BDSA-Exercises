beafb378c19924781d0335a3cd6c2b1ee5151eb9
Anders Wind Steffensen <awia00@gmail.com>
2014-10-16 20:39:50 +0200

    Created the first 3 reporting DTO classes: OrdersByTotalPriceDto UnitsSoldByMonth ProductsBySaleDto

---
+++ Northwind_Solution/Northwind/Reporting Module/OrdersByTotalPriceDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Reporting_Module
+{
+    class OrdersByTotalPriceDto
+    {
+        public OrdersByTotalPriceDto(int orderId, DateTime? orderDate, string customerContactName, decimal totalPriceWithDiscount, decimal totalPrice)
+        {
+            OrderId = orderId;
+            OrderDate = orderDate;
+            CustomerContactName = customerContactName;
+            TotalPriceWithDiscount = totalPriceWithDiscount;
+            TotalPrice = totalPrice;
+        }
+
+        public int OrderId { get; private set; }
+        public DateTime? OrderDate { get; private set; }
+        public string CustomerContactName { get; private set; }
+        public decimal TotalPriceWithDiscount { get; private set; }
+        public decimal TotalPrice { get; private set; }
+    }
+}
---
+++ Northwind_Solution/Northwind/Reporting Module/ProductsBySaleDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Reporting_Module
+{
+    class ProductsBySaleDto
+    {
+        public ProductsBySaleDto(int productId, string productName, IList<UnitsSoldByMonth> unitsSoldByMonth)
+        {
+            ProductId = productId;
+            ProductName = productName;
+            UnitsSoldByMonth = unitsSoldByMonth;
+        }
+
+        public int ProductId { get; private set; }
+        public string ProductName { get; private set; }
+        public IList<UnitsSoldByMonth> UnitsSoldByMonth { get; private set; }
+        //UnitsSoldByMonth [list of UnitsSold : int, Count : int, Month : int, Year : int]
+    }
+}
