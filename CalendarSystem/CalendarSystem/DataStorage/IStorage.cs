e20f58dcc8d34111e29ff62d3c921c586b20883f
Anders Wind Steffensen <awis@itu.dk>
2014-10-24 14:21:49 +0200

    Finished the pre and postconditions

--- CalendarSystem/CalendarSystem/DataStorage/IStorage.cs
+++ CalendarSystem/CalendarSystem/DataStorage/IStorage.cs
@@ -11,48 +11,46 @@ namespace CalendarSystem.DataStorage
     {
         /// <summary>
         /// Authenticate and download Calendar and events belonging to that user.
+        /// <para> @pre userName != null </para>
+        /// <para> @pre password != null </para>
+        /// <para> @pre username.match(password)</para>
+        /// <para> @pre exist(username)</para>
         /// </summary>
         /// <param name="userName"></param>
         /// <param name="password"></param>
-        /// <pre> userName != null </pre>
-        /// <pre> password != null </pre>
-        /// <pre> if(username !match password)</pre>
-        /// <pre> if(username !exist)</pre>
         /// <returns></returns>
         // TODO make pre and post conditions make method for autherization
         void loginAuthentication(string userName, string password);
 
+        // TODO sdd the post and pre here DONE       
         /// <summary>
         /// Save an event to the storage
+        /// <para>@pre eventToSave != null </para>
+        /// <para>@pre GetEvent(eventToSave.ID) == null </para>
+        /// <para>@pre eventToSave.ID &lt; 0 </para>
+        /// <para>@post GetAllEvents().Count == self@pre.GetAllEvents().Count + 1</para>
+        /// <para>@post GetEvent(eventToSave.ID) == eventToSave </para>
         /// </summary>
-        /// <param name="description"></param>
-        /// <param name="dateTime"></param>
-        /// <param name="timeSpan"></param>
-        /// <param name="notification"></param>
-        /// <pre> eventToSave != null </pre>
-        /// <pre> GetEvent(eventToSave.ID) == null </pre>
-        /// <pre> eventToSave.ID < 0 </pre>
-        /// <post> GetAllEvents().Count == self@pre.GetAllEvents().Count + 1 </post>
-        /// <post> GetEvent(eventToSave.ID) == eventToSave </post>
-        // TODO sdd the post and pre here DONE
+        /// <param name="eventToSave"></param>
         void SaveEvent(IEvent eventToSave);
 
         /// <summary>
         /// Update an event to the storage
+        /// <para> @pre eventToUpdate != null </para>
+        /// <para> @pre GetEvent(eventToUpdate.ID) != null </para> 
+        /// <para> @post getAllEvents().Count == self@pre.GetAllEvents().Count</para>
+        /// <para> @post GetEvent(eventToUpdate.ID) == eventToUpdate</para>
         /// </summary>
-        /// <param name="description"></param>
-        /// <param name="dateTime"></param>
-        /// <param name="timeSpan"></param>
-        /// <param name="notification"></param>
-        /// <pre> eventToUpdate != null </pre>
-        /// <pre> GetEvent(eventToUpdate.ID) != null </pre> 
-        /// <post> getAllEvents().Count == self@pre.GetAllEvents().Count</post>
-        /// <post> GetEvent(eventToUpdate.ID) == eventToUpdate</post>
+        /// <param name="eventToUpdate"></param>
         // TODO sdd the post and pre here DONE
         void UpdateEvent(IEvent eventToUpdate);
 
         /// <summary>
         /// Deletes the event with the given ID
+        /// <para> @pre GetEvent(ID) != null </para>
+        /// <para> @pre ID &gt; -1 </para> 
+        /// <para> @post GetEvent(ID) == null </para>
+        /// <para> @post getAllEvents().Count == self@pre.GetAllEvents().Count -1</para>
         /// </summary>
         /// <param name="ID">The ID of the </param>
         // TODO make pre and post conditions
@@ -68,22 +66,22 @@ namespace CalendarSystem.DataStorage
         /// Get a single event with a given ID if possible.
         /// </summary>
         /// <param name="ID"></param>
-        /// <returns></returns>
+        /// <returns>null if does not exist otherwise return an Event object</returns>
         IEvent GetEvent(int ID);
 
         /// <summary>
         /// Return all events between to given dates.
+        /// <para> @pre beginDateTime &lt; endDateTime </para> 
+        /// <para> @pre beginDateTime &gt;= new Date(1900,1,1) </para> 
+        /// <para> @pre endDateTime &gt;= new Date(1900,1,1) </para> 
+        /// <para> @pre beginDateTime &lt;= new Date(2100,1,1) </para> 
+        /// <para> @pre endDateTime &lt;= new Date(2100,1,1) </para> 
+        /// <para> @post return.TrueForAll(e=>e._date.Value >= beginDateTime && e._date.Value <= endDateTime)</para>
+        /// <para> @post return != null</para>
         /// </summary>
         /// <param name="beginDateTime"></param>
         /// <param name="endDateTime"></param>
-        /// <pre> beginDateTime < endDateTime </pre> 
-        /// <pre> beginDateTime >= new Date(1900,1,1) </pre> 
-        /// <pre> endDateTime >= new Date(1900,1,1) </pre> 
-        /// <pre> beginDateTime <= new Date(2100,1,1) </pre> 
-        /// <pre> endDateTime <= new Date(2100,1,1) </pre> 
-        /// <post> return.TrueForAll(e=>e._date.Value >= beginDateTime && e._date.Value <= endDateTime)</post>
-        /// <post> return != null</post>
-        /// <returns></returns>
+        /// <returns>A list of events</returns>
         // TODO sdd the post and pre here CHECK WITH TA check return in ocl
         IList<IEvent> GetEventsBetweenDates(DateTime beginDateTime, DateTime endDateTime);
 
