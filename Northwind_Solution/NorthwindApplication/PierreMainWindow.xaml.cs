feaf45e94dd5a805a075ffcf1dcd1c3afbd23391
Anders Wind Steffensen <awis@itu.dk>
2014-10-28 13:59:47 +0100

    Created the window classes for pierre and blundell

---
+++ Northwind_Solution/NorthwindApplication/PierreMainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NorthwindApplication
+{
+    /// <summary>
+    /// Interaction logic for PierreMainWindow.xaml
+    /// </summary>
+    public partial class PierreMainWindow : Window
+    {
+        public PierreMainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
