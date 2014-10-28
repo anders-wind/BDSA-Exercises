8b2a1360a5fffc576aa489f39d9b3aadad3b10da
Anders Wind Steffensen <awis@itu.dk>
2014-10-28 13:57:44 +0100

    Started creating the window for northwind application

--- Northwind_Solution/NorthwindApplication/MainWindow.xaml.cs
+++ Northwind_Solution/NorthwindApplication/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace NorthwindApplication
         {
             InitializeComponent();
         }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }
