ced789fb1fcee3f471cab0652d3e5a62a51c9050
Anders Wind Steffensen <awis@itu.dk>
2014-10-31 16:29:00 +0100

    Added a project for tests - and created 3 test classes one for each view model

---
+++ Northwind_Solution/NorthwindApplication.tests/OrderDetailsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+
+namespace NorthwindApplication.tests
+{
+    [TestFixture]
+    public class OrderDetailsViewModel
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Northwind_Solution/NorthwindApplication.tests/OrderViewModelTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+
+namespace NorthwindApplication.tests
+{
+    [TestFixture]
+    public class OrderViewModelTest
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
